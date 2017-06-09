be213f2fea1d4a12c223f8dac690dbb7a06ac0aa
Baran Tatar <barantatar@yahoo.com>
2017-06-09 17:08:16 -0400

    first commit

---
+++ inventory/views.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+
+from inventory.models import Item
+
+
+def index(request):
+    items = Item.objects.exclude(amount=0)
+    return render(request, 'inventory/index.html', {
+        'items': items,
+    })
+
+
+def item_detail(request, id):
+    try:
+        item = Item.objects.get(id=id)
+    except Item.DoesNotExist:
+        raise Http404('This item does not exist')
+    return render(request, 'inventory/item_detail.html', {
+        'item': item,
+    })
